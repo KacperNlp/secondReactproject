a9af6af4d9b3acfdf93e8e7cdfdfeecdde9ad96a
KacperNlp <kacper.nlp@wp.pl>
2020-08-06 15:35:50 +0200

    Updates

---
+++ precache-manifest.8ac15e33f50f465f92d00eb77c56a47f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc8f70a16696c657a95695476087f40",
+    "url": "/secondReactproject/index.html"
+  },
+  {
+    "revision": "61ec8b5b4da68bc7ea54",
+    "url": "/secondReactproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/secondReactproject/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/secondReactproject/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ec8b5b4da68bc7ea54",
+    "url": "/secondReactproject/static/js/main.b8266f12.chunk.js"
+  },
+  {
+    "revision": "1cf9b1ae8cc751643ee9",
+    "url": "/secondReactproject/static/js/runtime-main.3d9253ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/secondReactproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
